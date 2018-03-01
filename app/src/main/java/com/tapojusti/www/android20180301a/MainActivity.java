2b0dfa1a2ea293670d1a605d6fcb29bb8206e9b1
tapojusti <mastechinas@gmail.com>
2018-03-01 11:25:57 +0200

    Initial commit

---
+++ app/src/main/java/com/tapojusti/www/android20180301a/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tapojusti.www.android20180301a;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZP17-2 on 2018.03.01.
+ */
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
